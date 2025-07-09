7b2c0f1d265703af3f0c6e98212dcfceadf4e264
aswathy <aswathyshaji22@gmail.com>
2025-07-09 12:58:28 +0530

    path change in urls.py

--- ebook/urls.py
+++ ebook/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect  # 👈 Add this import
 
 urlpatterns = [
+    path('', lambda request: redirect('/api/books/')),  # 👈 Redirect root URL
     path('admin/', admin.site.urls),
     path('api/', include('books.urls')),
 ]
